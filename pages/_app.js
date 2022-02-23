6bb04d22c2946404cd258a997c655c51ac894146
ShigeoShinohara <s.shinohara@sync-web.jp>
2022-02-23 14:25:14 +0900

    add about pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
+// 共通の処理を書く時にはここに書く
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <p>I&apos;m a Header!!!</p>
+        <Component {...pageProps} />
+      </>
+      )
 }
 
 export default MyApp
---
+++ pages/about.js
@@ -0,0 +1,5 @@
+function about() {
+    return 'I am an about page'
+}
+
+export default about;
