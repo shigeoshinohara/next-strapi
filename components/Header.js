e120ca96510c0161647af6ad506b4e7b2435ef8b
ShigeoShinohara <s.shinohara@sync-web.jp>
2022-02-23 14:31:29 +0900

    add Header

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+        <header>
+            Now I AM a real Header
+        </header>
+    )
+}
+export default Header
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
+import Header from "../components/Header";
 
 // 共通の処理を書く時にはここに書く
 function MyApp({ Component, pageProps }) {
   return (
       <>
-        <p>I&apos;m a Header!!!</p>
+        <Header/>
         <Component {...pageProps} />
       </>
       )
