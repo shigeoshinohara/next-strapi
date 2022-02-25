47d09a646fa5ff45ab23fa102ccf09fb39799681
ShigeoShinohara <s.shinohara@sync-web.jp>
2022-02-25 15:34:31 +0900

    CSS in JS

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,34 @@
-function Header() {
-    return (
-        <header>
-            Now I AM a real Header
-        </header>
-    )
+import styled from "@emotion/styled";
+
+function Header({ isDark }) {
+  return (
+    <HeaderStyled isDark={isDark}>
+      <div className="container">
+        <div className="logo">
+          <img src="/images/logo.svg" alt="Site logo" />
+          <span className="logo-text">Next Movies</span>
+        </div>
+      </div>
+    </HeaderStyled>
+  );
 }
-export default Header
+
+const HeaderStyled = styled.header`
+  // background: ${(props) => props.theme.colors.primary};
+  background: ${(props) => (props.isDark ? "#000000" : "#efefef")};
+  padding: 20px;
+
+  .logo {
+    display: flex;
+    align-items: center;
+
+    .logo-text {
+      color: #333333;
+      font-weight: bold;
+      font-size: 20px;
+      margin-left: 20px;
+    }
+  }
+`;
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,25 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import Header from "../components/Header";
+import { ThemeProvider } from "@emotion/react";
+import GlobalStyles from "../components/GlobalStyles/GlobalStyles";
+
+const theme = {
+  colors: {
+    primary: "#efefef",
+  },
+};
 
 // 共通の処理を書く時にはここに書く
 function MyApp({ Component, pageProps }) {
   return (
-      <>
-        <Header/>
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Header />
         <Component {...pageProps} />
-      </>
-      )
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
